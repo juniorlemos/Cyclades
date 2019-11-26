8e82904e7232c18f1c7854b3a29516dc5dc45581
juniorlemos <47072463+juniorlemos@users.noreply.github.com>
2019-11-26 16:39:37 -0300

    Arquivos bases do cyclades

---
+++ Deuses.java
@@ -0,0 +1,24 @@
+
+import  java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+
+public class Deuses
+{
+  
+  List<String> deuses;
+
+ 
+  public void chamar(ArrayList<String> deuses){
+Collections.shuffle(deuses);
+System.out.println(deuses);
+
+}
+  
+  
+
+ 
+  
+}
+
---
+++ Main.java
@@ -0,0 +1,20 @@
+import  java.util.ArrayList;
+public class Main
+{
+  // arguments are passed using the text field below this editor
+  public static void main(String[] args)
+  {
+   
+Deuses d = new Deuses();
+ ArrayList<String> deuses = new ArrayList<String>();
+ deuses.add("ZEUS");
+ deuses.add("POSSEIDON");
+ deuses.add("ARES");
+ deuses.add("ATHENA");
+ deuses.add("KRONOS");
+
+
+
+d.chamar(deuses);
+  }
+}
